3effff2e547e32b936b1c7863d8a994a198e48d2
RomanFirstt <roman.asalkhanov@gmail.com>
2023-07-02 14:17:41 +0200

    up routes

---
+++ pages/test.tsx
@@ -0,0 +1,12 @@
+const Test = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci libero
+      ipsa recusandae pariatur. Quibusdam veniam natus quos reprehenderit
+      asperiores rerum, aut rem fugit, aliquam expedita tenetur amet nesciunt ad
+      quaerat!
+    </div>
+  );
+};
+
+export default Test;
